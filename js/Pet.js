d049e8e19e6781c405466746b34c4c90e8404898
Haike <haike16@hotmail.com>
2024-10-27 11:04:10 +0100

    Functionality added to Pet.js

--- js/Pet.js
+++ js/Pet.js
@@ -7,4 +7,34 @@ export class Pet {
       this.petDetail = petDetail;
       this.energyLevel = energyLevel;
    }
+
+   feedPet() {
+      console.log('fed')
+      if (this.energyLevel + 10 >= 100) {
+         this.energyLevel = 100;
+      } else {
+         this.energyLevel += 10;
+      }
+
+      console.log(this.energyLevel)
+      return this.energyLevel;
+   }
+
+   playWithPet() {
+      console.log('play')
+
+      if (this.energyLevel - 10 <= 0) {
+         this.energyLevel = 0
+      } else {
+         this.energyLevel -= 10;
+      }
+
+      console.log(this.energyLevel)
+      return this.energyLevel;
+   }
+
+   deletePet(petId) {
+      const ul = document.getElementById(petId)
+      ul.remove();
+   }
 }
