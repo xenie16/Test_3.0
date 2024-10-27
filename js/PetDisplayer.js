684c69751a7c305a739b6c617305175ec3342636
Haike <haike16@hotmail.com>
2024-10-27 16:20:22 +0100

    added buttons for all pets

--- js/PetDisplayer.js
+++ js/PetDisplayer.js
@@ -1,6 +1,36 @@
 "use strict";
 
 export class PetDisplayer {
+
+   constructor(allPets) {
+      this.allPets = allPets;
+   }
+
+   // showAllPets(allPets) {
+   //    this.allPets = allPets;
+
+   //    const ourPetsTitle = document.getElementById('ourPetsTitle');
+
+   //    for (const pet in this.allPets) {
+   //       const petType = this.allPets[pet].petType;
+   //       const petName = this.allPets[pet].petName;
+   //       const petDetail = this.allPets[pet].petDetail;
+   //       const energyLevel = this.allPets[pet].energyLevel;
+
+   //       const ul = document.createElement('ul');
+   //       console.log(ul)
+   //       ourPetsTitle.insertAdjacentElement("afterend", ul);
+
+   //       const petCard = `
+   //       <li><span class="bold">Pet type: </span>${petType}</li>
+   //       <li><span class="bold">Name: </span>${petName}</li>
+   //       <li><span class="bold">Details: </span>${petDetail}</li>
+   //       <li><span class="bold">Energy: </span>${energyLevel}</li>
+   //       `
+   //       ul.insertAdjacentHTML('beforeend', petCard);
+   //    }
+   // }
+
    showPet(newPet) {
       const allPetsSection = document.getElementById('allPetsSection');
       const ul = document.createElement('ul');
@@ -41,9 +71,21 @@ export class PetDisplayer {
    }
 
    addEventListeners(newPet, petId) {
+      const playWithAllButton = document.getElementById('playWithAllButton');
+      const feedAllButton = document.getElementById('feedAllButton');
+
       const playPetButton = document.getElementById(`play-${petId}`);
       const feedPetButton = document.getElementById(`feed-${petId}`);
       const deletePetButton = document.getElementById(`delete-${petId}`);
+
+      playWithAllButton.addEventListener("click", () => {
+         const newEnergyLevel = newPet.playWithPet();
+         this.updateEnergy({ newEnergyLevel });
+      })
+
+      feedAllButton.addEventListener("click", () => {
+
+      })
 
       playPetButton.addEventListener("click", () => {
          const newEnergyLevel = newPet.playWithPet()
@@ -61,7 +103,17 @@ export class PetDisplayer {
    }
 
    updateEnergy({ petId, newEnergyLevel }) {
-      const energyLevelLi = document.querySelector(`#${petId} .energyLevel`);
-      energyLevelLi.textContent = `energyLevel: ${newEnergyLevel}`
+      let energyLevelLi;
+      console.log(newEnergyLevel)
+
+      if (petId) {
+         energyLevelLi = document.querySelector(`#${petId} .energyLevel`);
+         energyLevelLi.textContent = `energyLevel: ${newEnergyLevel}`
+      } else {
+         energyLevelLi = document.querySelectorAll('.energyLevel');
+         energyLevelLi.forEach(liItem => {
+            liItem.textContent = `energyLevel: ${newEnergyLevel}`
+         })
+      }
    }
 }
